4e8842fb6741f7972f7402941c3888e1ad91a31b
Ori Baram <obrm770@gmail.com>
2023-01-30 19:21:54 +0200

    update

--- 2_OOP_in_practice/script.js
+++ 2_OOP_in_practice/script.js
@@ -27,26 +27,102 @@ class Recipe {
   }
 }
 
-// Create an array of ingredients
-const ingredients = [
-  new Ingredient('Eggs'),
-  new Ingredient('Bacon'),
-  new Ingredient('Bread'),
-  new Ingredient('Cheese'),
-  new Ingredient('Tomatoes')
-];
+class RecipeFinder {
+  constructor() {
+    this.ingredients = [
+      new Ingredient('Chicken'), // 0
+      new Ingredient('Beef'), // 1
+      new Ingredient('Tofu'), // 2
+      new Ingredient('Broccoli'), // 3
+      new Ingredient('Carrots'), // 4 
+      new Ingredient('Spinach'), // 5
+      new Ingredient('Rice'), // 6 
+      new Ingredient('Pasta'), // 7
+      new Ingredient('Bread'), // 8
+    ];
 
-// Add a click event listener to the "Generate Recipe" button
-document.querySelector('#generate-recipe-button').addEventListener('click', () => {
-  // Filter the ingredients to only include the selected ones
-  const selectedIngredients = ingredients.filter(ingredient => ingredient.selected);
-  // Create a recipe using the selected ingredients
-  const recipe = Recipe.createRecipe(selectedIngredients);
-  if (recipe instanceof Recipe) {
-    // If a recipe was created, log the ingredients used
-    console.log(`Recipe created with ingredients: ${recipe.ingredients.map(ingredient => ingredient.name).join(', ')}`);
+    this.recipes = [
+      new Recipe([
+        this.ingredients[0],
+        this.ingredients[3],
+        this.ingredients[4],
+        this.ingredients[6]
+      ]),
+      new Recipe([
+        this.ingredients[1],
+        this.ingredients[4],
+        this.ingredients[7],
+        this.ingredients[8]
+      ]),
+      new Recipe([
+        this.ingredients[2],
+        this.ingredients[3],
+        this.ingredients[5],
+        this.ingredients[6]
+      ])
+    ];
+  }
+
+  findRecipe(selectedIngredients) {
+    // Loop through all the recipes
+    for (const recipe of this.recipes) {
+      // Check if all the ingredients in the recipe are also selected by the user
+      let isMatch = true;
+      for (const ingredient of recipe.ingredients) {
+        if (!selectedIngredients.includes(ingredient)) {
+          isMatch = false;
+          break;
+        }
+      }
+
+      // If all the ingredients in the recipe are selected, return the recipe
+      if (isMatch) {
+        return recipe;
+      }
+    }
+
+    // If no recipe was found, return null
+    return null;
+  }
+}
+
+// Initialize the RecipeFinder instance
+const recipeFinder = new RecipeFinder();
+
+// Get the checkbox elements
+const checkboxes = document.querySelectorAll(".ingredient-checkbox");
+
+// Add event listeners to the checkboxes to update the selected ingredients list when they change
+const selectedIngredients = [];
+
+for (const checkbox of checkboxes) {
+  checkbox.addEventListener("change", event => {
+    const ingredientName = event.target.value;
+    const ingredient = recipeFinder.ingredients.find(
+      ingredient => ingredient.name === ingredientName
+    );
+
+    if (event.target.checked) {
+      selectedIngredients.push(ingredient);
+    } else {
+      selectedIngredients.splice(selectedIngredients.indexOf(ingredient), 1);
+    }
+  });
+}
+
+// Get the button element
+const button = document.querySelector("#submit");
+
+// Add event listener to the button to find the recipe when it's clicked
+button.addEventListener("click", () => {
+  // Find the recipe with the selected ingredients
+  const recipe = recipeFinder.findRecipe(selectedIngredients);
+
+  // Check if a recipe was found
+  if (recipe) {
+    document.getElementById('recipeModal').classList.add('show');
   } else {
-    // If an error message was returned, log the error message
-    console.log(recipe);
+    document.getElementById('errorToast').classList.add('show');
+    //
   }
 });
